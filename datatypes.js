5e2495ba4993781c4103877245a2d56f82019e74
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-12-19 10:45:08 +0330

    Objects, Arrays

--- datatypes.js
+++ datatypes.js
@@ -92,3 +92,48 @@ console.log((firstBool == secondBool));
 ++firstBool;
 console.log((firstBool == secondBool));
 console.log((firstBool !== secondBool));
+
+/**
+ * 
+ *      Arrays in JS
+ * 
+ *      JavaScript Arrays are written with square barackets (Same as the C programming language)
+ *      Array Items are seperated by commas.
+ *      The following code declares (creates) an array called humen, containing three items (people names)
+ * 
+ */
+
+const humen = ["John", "Tommy", "Ash"];
+var indexData = "";
+let somethingToSeeBlyad = 19 + " ";
+console.log(humen[0]);
+humen.push("Jerry");
+
+for (let index = 0; index < humen.length; index++) {
+    indexData += humen[index];
+    indexData += " ";
+}
+console.log(indexData);
+
+console.log(19 + " ");
+console.log(typeof somethingToSeeBlyad);
+
+/**
+ * 
+ *      JS Objects 
+ *      JavaScript objects are written in Curly braces {}
+ *      Object properties are written as name: value pairs, seperated by commas.
+ */
+
+const animal =  {firstName: "Jessy", age: "8", eyeColor: "black"};
+animal.nationality = undefined;
+animal.nationality = animal.nationality ? ("England") : "Norway";
+console.log(animal);
+console.log(animal.nationality);
+
+animal.child = null;
+animal.child ??= "root";
+console.log(animal.child);
+
+
+// An empty string value has nothing to with undefined 
