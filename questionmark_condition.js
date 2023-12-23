abe314c9d694f1a0aea32d84333096e47348053c
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-12-23 13:48:45 +0330

    Some examples using the Ternary operator (QuestionMark Conditions) in JS

--- questionmark_condition.js
+++ questionmark_condition.js
@@ -25,6 +25,23 @@ function returnFalse() {
 }
 
 let falseValue = returnFalse();
-if (falseValue === 0) {
-    console.log("the value is false");
-}
+let result = (falseValue === 0) ? false : true;
+console.log(`the result is : ${result}`);
+
+
+let age = 19;
+let user_age = (age < 15) ? "a teenage" :
+                (age < 15) ? "a kid" :
+                (age === 19) ? "19" :
+                "dead";
+console.log(`the user is ${user_age}`);
+
+let checkItsTrue = true;
+(checkItsTrue === true) ? console.log("it is true") : console.log("It is false");
+
+
+let a = 2, b = 4;
+let sum_result;
+(a + b > 5) ? sum_result = "Over" : sum_result = "Below";
+console.log(`the value of sum_result is : ${sum_result}`);
+
