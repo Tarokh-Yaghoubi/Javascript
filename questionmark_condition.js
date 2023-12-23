30454eca9fc6d76337ab2eccae3cb28ea92569ae
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-12-23 11:50:49 +0330

    I was curious about user-input in NodeJS

--- questionmark_condition.js
+++ questionmark_condition.js
@@ -1,2 +1,30 @@
 
 // Conditional Branching: if, '?'
+
+
+/**
+ * 
+ *      Sometimes, we need to perform different actions based on different conditions.
+ *      To do that, we can use the if statement and the conditional operator ?, that's 
+ *      also called a "question mark" operator.
+ * 
+ */
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('What is your name ? ', name => {
+    console.log(`Hello miss/mister ${name}`);
+    readline.close();
+});
+
+function returnFalse() {
+    return 0;
+}
+
+let falseValue = returnFalse();
+if (falseValue === 0) {
+    console.log("the value is false");
+}
