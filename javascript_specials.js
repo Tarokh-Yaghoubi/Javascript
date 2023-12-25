7220af7e48ec53e92d282af9afcdc411f057ca4b
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-12-25 16:28:53 +0330

    Javascript already-known concepts short-review

--- javascript_specials.js
+++ javascript_specials.js
@@ -8,3 +8,9 @@ console.log("first"); console.log("second");
 console.log("third")
 console.log("fourth")       // these two are different statements 
 
+// It is a better practice to put a semicolon 
+
+// even if we put some extra semi-colon somewhere in the code 
+// it will not be an error, it will be just ignored 
+
+// to fully enable all features of modern JS, we should use 'use strict' at the beginning of the scripts 
