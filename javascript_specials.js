8e6c493964cd4a3c888251e06a33fcfdec10e1da
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-12-25 16:22:45 +0330

    Already known concepts related to JavaScript

---
+++ javascript_specials.js
@@ -0,0 +1,10 @@
+
+
+// Semicolon is a delimiter in JS 
+// Also line-break is treated as a delimiter, so that would also work
+
+console.log("first"); console.log("second");
+
+console.log("third")
+console.log("fourth")       // these two are different statements 
+
