d85d3627d89450350ff0be56017e76ba4381a478
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-12-26 13:36:09 +0330

    This is going to be the entry point of this JS repo

---
+++ app.js
@@ -0,0 +1,7 @@
+const toCelsius = require("../Javascript/functions")
+
+
+let fahrenheit = 123;
+let answer = toCelsius(fahrenheit);
+
+console.log(`The answer of toCelsius in 123 Fahrenheit is ${answer}`);
