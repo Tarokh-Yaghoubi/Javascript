8b04fb4c47793e7edcf2792fd9f5006a4ca9f628
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2023-12-27 11:06:22 +0330

    Objected references and copying | JS | Objects

--- Objects/object_references_copying.js
+++ Objects/object_references_copying.js
@@ -34,3 +34,41 @@ console.log(admin.firstName);   // they are both the same
 
 
 for (let key in clone) console.log(clone[key]);
+
+let someObj = Object.assign({}, admin);
+console.log(someObj);
+
+let C_Programmer = {
+    name: "tarokh",
+    age: 19,
+    company: "EniacTech",
+    func: function() {
+        console.log(this.company);
+    },
+    languages: {
+        low_level_lang: "C programming language",
+        script_lang: "JavaScript",
+        human_lang: "English and Persian"
+    }
+};
+
+console.log(C_Programmer);
+// let C_Programmer_Clone = Object.assign({}, C_Programmer);
+let C_Programmer_Clone = {};
+for (let key in C_Programmer) {
+    if (typeof C_Programmer[key] !== "function") C_Programmer_Clone[key] = C_Programmer[key];
+}
+console.log(C_Programmer_Clone);
+// C_Programmer_Clone.func();
+
+let anotherClone = Object.assign({}, C_Programmer);
+console.log(anotherClone);
+
+
+let newUser = new Object(); 
+newUser.me = newUser;
+
+let newClone = structuredClone(newUser);
+console.log(newClone);
+
+
