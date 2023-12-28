89559143a6fd743a9238f17de8b4a29db220ca7c
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-12-28 19:25:41 +0330

    added some changed to the file

--- datatypes_2.js
+++ datatypes_2.js
@@ -8,3 +8,29 @@
 
 console.log(Infinity);
 console.log(typeof Infinity);
+
+let name = "tarokh";
+console.log(`The name of our programmer is : ${name}`);
+
+/**
+ * 
+ *      In some languages we have the char type, like in the C programming language 
+ *      or in Java. But in JS, we do not have this type, we only have strings.
+ * 
+ */
+
+let isGreater = 4 > 3;
+console.log(isGreater);     // true 
+
+
+// If a value is declared but not defined, its value is undefined
+
+let somethingUndefined;     // now, this is undefined
+let somethingUndefinedDirectly = undefined;     // this is also undefined
+
+console.log(somethingUndefined, somethingUndefinedDirectly);
+
+console.log(typeof alert);
+console.log(typeof null);
+console.log(typeof undefined);
+
