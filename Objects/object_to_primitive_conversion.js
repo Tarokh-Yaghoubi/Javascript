2c513513a2203ca71a6a9583f90f519e9d1aaf2a
Tarokh Yaghoubi <tarokhgit@gmail.com>
2023-12-29 21:49:41 +0330

    stage commit

---
+++ Objects/object_to_primitive_conversion.js
@@ -0,0 +1,26 @@
+
+/**
+ * 
+ *      JS does not allow you to customize how operators work on Objects 
+ *      Unlike some other languages, such as C++ or Ruby, we cannot implement
+ *      a special object method to handle addition (or other operators).
+ * 
+ */
+
+/**
+ * 
+ *      In case of such operations, objects are auto-converted to primitives, and then 
+ *      the operation is carried out using this primitives and resutls in a primitive value
+ * 
+ */
+
+/**
+ * 
+ *      That is an important limitation, the result of obj1 + obj2 can not be another object 
+ *      We cannot make objects representing vectors or matrices (or achievments or whatever),
+ *      add them and expect a "summed" object as the result . such architectural feats are automatically 
+ *      "off the board".
+ * 
+ * 
+ */
+
