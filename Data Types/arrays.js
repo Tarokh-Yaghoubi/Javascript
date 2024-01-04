b7fc1a2510b9a1beb0541b885c69a820bfe7cd32
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-01-05 00:51:35 +0330

    Played with arrays a little

---
+++ Data Types/arrays.js
@@ -0,0 +1,59 @@
+
+let arr = new Array();
+let arr1 = [];      // these are the two ways for creating arrays
+// almost all the time the second syntax is used
+
+let fruits = ["Apple", "Orange", "Banana"];
+
+// array elements are numbered, starting with zero 
+
+console.log(fruits[0]);
+fruits[0] = "Coconuts";
+console.log(fruits);
+
+fruits[3] = "Cherry";
+
+console.log(fruits);
+console.log(fruits.length); // this will give 4, because we have 4 items.
+console.log(fruits[fruits.length - 1]);     // this will show cherry which is the last index of the array
+
+
+// An array can store elements of any type, for example:
+
+let anyTypeArray = ["firstname", true, {color: "black"}, false, function(){console.log("correct");}, 54, (a, b)=>console.log(`sum of nums is : ${a + b}`)];
+console.log(anyTypeArray);
+anyTypeArray[4]();
+anyTypeArray[anyTypeArray.length - 1](3, 2);
+console.log(anyTypeArray[2].color);
+
+
+// fruits[-1] , we cannot access the last index like this (unlike python)
+// we have to use the .at method (it is not supported by the old versions of node and browsers)
+
+let lastIndex = anyTypeArray.at(-1);    // accessing the last element which is an arrow function
+lastIndex(5, 6);
+
+
+anyTypeArray.push((a, b, c) => (console.log(`a + b * c results : [${a + b * c}]`)));       // push an arrow function to the end of the array
+
+anyTypeArray.at(-1)(4, 2, 6);
+
+anyTypeArray.shift();
+anyTypeArray.unshift("tarokh");
+
+
+let copyArray = anyTypeArray;
+console.log(copyArray === anyTypeArray);        // this is true, it is copied by reference 
+
+copyArray.push("C Developer");
+console.log(anyTypeArray.at(-1));       // this is also changed due to copyArray push ...
+
+
+let copyByValue = [];
+for (let char in anyTypeArray) copyByValue[char] = anyTypeArray[char];
+console.log("------------------- -----------------");
+console.log(copyByValue == anyTypeArray);   // this is false ! , it is strange ! 
+console.log(copyByValue);
+console.log(anyTypeArray);
+copyByValue.at(-2)(9, 54, 6);
+
