982fdf0d582033db43117fe9d1dedc018399f2e7
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-01-29 00:51:33 +0330

    differences between async and sync | JS | Second commit

--- Async_Await/async_and_await.js
+++ Async_Await/async_and_await.js
@@ -48,3 +48,40 @@
 
 // JAVASCRIPT
 
+// JS is an asynchronous and cocurrent programming language that offers a lot of flexiblity.
+// It is single-threaded synchronous and also non-blocking like asynchronous.
+
+// Altough it is synchronous by nature, JavaScript benefits from an Asynchronous process.
+// Long-running JavaScript functions can make the USER INTERFACE (UI) or server unresponsive until 
+// the function has returned, resulting in a less-than-stellar user experience.
+
+// However, there are some instances where users can benefit from blocking programming, for example; when making an "online payment".
+
+// JS offers both single-thread and multi-thread, blocking and non-blocking.
+
+
+// SYNCHRONOUS VS ASYNCHRONOUS PROGRAMMING
+
+// Ultimately, the choice comes down to operational dependencies. Do you want the start of an operation to depend
+// on another operations completion, or do you want it to run independently.
+
+// ASYNCHRONOUS is a non-blocking architecture, so the execution of one task is not dependent on another. Tasks can run simultaneously.
+
+// SYNCHRONOUS is a blocking architecture, so the execution of each operation depends on completing the one before it.
+// each task requires an answer before moving on to the next iteration.
+
+// async is multi-threaded, which means operations and programs can run in parallel.
+// sync is a single-thread, so only one application or program will run at a time.
+
+// async is non-blocking, which means it will send multiple requests to a server at a time.
+// sync is blocking - it will send the server one request at a time and wait for that request to be answered by the server.
+// sync is slower.
+
+
+/**
+ * 
+ *      DIFFERENCES ASIDE, ASYCN AND SYNC methods have advantages for different stakeholders: async for users and sync for developers.
+ * 
+ */
+
+// Asynchronous programming enhances the user experience by decreasing the lag time 
