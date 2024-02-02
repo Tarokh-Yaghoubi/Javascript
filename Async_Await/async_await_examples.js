d28d2833558cf9e53de8067d314876e4ade84936
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-02-02 20:38:53 +0330

    Examples with async and await in JS | third commit

--- Async_Await/async_await_examples.js
+++ Async_Await/async_await_examples.js
@@ -110,3 +110,74 @@ async function fetchData() {
 
 fetchData();
 
+// IN JavaScript many operations take time to complete, such as fetching data from a server, reading files, or waiting for user input.
+// During these operations, your code continues to execute, and it is essential to handle these asynchronous operations effectively.
+
+// Traditionally JavaScript used callbacks to handle asynchronous tasks, However, this approach often led to complex and hard-to-read code,
+// known as "callback hell".
+
+// To simplify asynchronous programming, ES6 introduced 'async' and 'await' keywords. which makes asynchronous code more readable and manageable
+
+/**
+ * 
+ *      ASYNC FUNCTION
+ * 
+ *      The 'async' keyword is used to declare that a function returns a promise and allows you to use the 'await' keyword within it,
+ *      when you declare a function as 'async', it means that the function will always return a promise, whether you explicitly return a promise or not.
+ * 
+ */
+
+async function fetchMoreData() {
+    return 'Data Fetched!';
+}
+
+/**
+ * 
+ *      'await' operator
+ * 
+ *      The await keyword is used to pause the execution of an 'async' function until a promise is settled (resolved or rejected).
+ *      It makes JavaScript wait until the promise returns a result.
+ *  
+ */
+
+async function fetchDataFromServer() {
+    let data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    let json = await data.json();
+    return json;
+}
+
+
+async function fetchAnotherData() {
+    let data = await fetchDataFromServer();
+    console.log(`FETCH ANOTHER DATA: ${data}`);
+}
+
+fetchAnotherData();
+
+
+
+async function fetchFinalData() {
+
+    try {
+
+        let response = await fetch("https://jsonplaceholder.typicode.com/todos/3");
+        
+        if (response.status === 200) {
+
+            let data = await response.json();
+            console.log(data);
+
+        } else {
+
+            throw new Error("Error occured");
+        
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+
+fetchFinalData();
+
